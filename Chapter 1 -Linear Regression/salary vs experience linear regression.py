f4805bcb2b32cb6a1932e04eb23d26a2e94e8325
rinabaral <rina12baral@gmail.com>
2020-05-06 23:17:36 +0545

    Linear Regression

---
+++ Chapter 1 -Linear Regression/salary vs experience linear regression.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+#dataset = pd.read_csv('181105_missing-data.csv')
+dataset = pd.read_csv('C:\Python\salary_data.csv')
+X = dataset.iloc[:, :-1].values #get a copy of dataset exclude last column
+y = dataset.iloc[:, 1].values #get array of dataset in column 1st
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1/3, random_state=0)
+
+"""
+# Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+"""
+
+# Fitting Simple Linear Regression to the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = regressor.predict(X_test)
+
+# Visualizing the Training set results
+plt.scatter(X_train, y_train, color='red')
+plt.plot(X_train, regressor.predict(X_train), color='blue')
+plt.title('Salary VS Experience (Training set)')
+plt.xlabel('Year of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualizing the Test set results
+plt.scatter(X_test, y_test, color='red')
+plt.plot(X_train, regressor.predict(X_train), color='blue')
+plt.title('Salary VS Experience (Test set)')
+plt.xlabel('Year of Experience')
+plt.ylabel('Salary')
+plt.show()
